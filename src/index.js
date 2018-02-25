bffa697adb6bde068efe91c703d076f4316c4fdd
Dmitry <981.m.dmitriy@gmail.com>
2018-02-25 15:47:18 +0300

    76%

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    var n = 2;
+    var changed = base;
+    var needValue;
+    var zerosCount=0;
+    
+    while (n <= changed) {
+        if (changed % n == 0) {
+            needValue = changed;
+            changed = Math.round(changed / n);
+        } else {
+            n++;
+        }
+    }
+    
+    for (var i = 1; i < 12; i++) {
+        zerosCount += Math.floor(number / Math.pow(needValue, i));
+    }
+    
+    return zerosCount;
 }
